06127add10465ff5232eeea7be7a82d7f30dcfc0
Edon Gashi <edon.gashi@uni-pr.edu>
2019-04-13 15:08:30 +0200

    indentation

--- src/Timetabling.Common/Utils.fs
+++ src/Timetabling.Common/Utils.fs
@@ -6,9 +6,9 @@ module Option =
   let rtn v = Some v
 
   let traverseList f ls =
-      let folder head tail =
-        f head >>= (fun h -> tail >>= (fun t -> h :: t |> rtn))
-      List.foldBack folder ls (rtn List.empty)
+    let folder head tail =
+      f head >>= (fun h -> tail >>= (fun t -> h :: t |> rtn))
+    List.foldBack folder ls (rtn List.empty)
 
   let sequenceList ls = traverseList id ls
 
@@ -18,9 +18,9 @@ module Result =
   let rtn v = Ok v
 
   let traverseList f ls =
-      let folder head tail =
-        f head >>= (fun h -> tail >>= (fun t -> h :: t |> rtn))
-      List.foldBack folder ls (rtn List.empty)
+    let folder head tail =
+      f head >>= (fun h -> tail >>= (fun t -> h :: t |> rtn))
+    List.foldBack folder ls (rtn List.empty)
 
   let sequenceList ls = traverseList id ls
 
