72ae4c614858bdb49b13a1d3d5290008a67fda38
Edon Gashi <edon.gashi@uni-pr.edu>
2019-04-16 16:52:41 +0200

    added cli project

---
+++ src/Timetabling.CLI/Program.fs
@@ -0,0 +1,11 @@
+﻿open Timetabling.Common
+open System.Xml.Linq
+
+[<EntryPoint>]
+let main args =
+  let xdoc = XDocument.Load(@"D:\Documents\inst\demo\bet-sum18.xml")
+  let x = Parse.problem xdoc.Root
+  match x with
+  | Ok x -> printfn "Ok! %A" x
+  | Error x -> printfn "Error %A" x
+  0
